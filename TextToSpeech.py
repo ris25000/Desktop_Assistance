f136292c5e31aa2c7af6964324e0aec0dac0f14f
rishabh <rispan25000@gmail.com>
2024-04-26 10:57:50 +0530

    test

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -3,8 +3,8 @@ import pyttsx3
 class TextToSpeech:
     def __init__(self):
         self.engine = pyttsx3.init()
-        self.engine.setProperty('voice', 'english')  # Use a default English voice
-        self.engine.setProperty('rate', 180)  # Increase the speaking rate for clearer voice
+        self.engine.setProperty('voice', 'english_in')  # Use a default English voice
+        self.engine.setProperty('rate',200 )  # Increase the speaking rate for clearer voice
 
     def tospeech(self, text):
         self.engine.say(text)
